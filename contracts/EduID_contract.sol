87b00eedb92066d1e726246beb62cac6071e6f3b
Remix Workspace <>
2022-09-19 14:14:46 +0800

    ISSC/Update Add initInstitutionMap function with mapping

--- contracts/EduID_contract.sol
+++ contracts/EduID_contract.sol
@@ -10,12 +10,13 @@ contract ISSC {
         string jenisSekolah;
         string alamatInstitusi;
         uint nomorTelepon;
-        bool agreeToJoin;
     }
 
     InstitutionInfo[] public institutionInfoMap;
 
+    // mapping address to Institution Info
     mapping(address => InstitutionInfo) public PubKeyInstitutionInfoMap;
+    address[] public institutionBlockAddressArray;
 
     function initInstitutionMap(
         address blockAddress, 
@@ -24,35 +25,59 @@ contract ISSC {
         string memory _akreditasi,
         string memory _jenisSekolah,
         string memory _alamatInstitusi,
-        uint _nomorTelepon,
-        bool _agreeToJoin) public returns (bool) {
-            blockAddress = msg.sender;
+        uint _nomorTelepon) public returns (bool) {
+            
+            require(blockAddress == msg.sender);
 
-            institutionInfoMap.push(InstitutionInfo({
-                namaInstitusi: _namaInstitusi,
-                NPSN : _NPSN,
-                akreditasi : _akreditasi,
-                jenisSekolah : _jenisSekolah,
-                alamatInstitusi : _alamatInstitusi,
-                nomorTelepon : _nomorTelepon,
-                agreeToJoin : _agreeToJoin
-            }));
+            // institutionInfoMap.push(InstitutionInfo({
+            //     namaInstitusi: _namaInstitusi,
+            //     NPSN : _NPSN,
+            //     akreditasi : _akreditasi,
+            //     jenisSekolah : _jenisSekolah,
+            //     alamatInstitusi : _alamatInstitusi,
+            //     nomorTelepon : _nomorTelepon,
+            //     agreeToJoin : _agreeToJoin
+            // }));
+
+            //append data to mapping
+            PubKeyInstitutionInfoMap[blockAddress].namaInstitusi = _namaInstitusi;
+            PubKeyInstitutionInfoMap[blockAddress].NPSN = _NPSN;
+            PubKeyInstitutionInfoMap[blockAddress].akreditasi = _akreditasi;
+            PubKeyInstitutionInfoMap[blockAddress].jenisSekolah = _jenisSekolah;
+            PubKeyInstitutionInfoMap[blockAddress].alamatInstitusi = _alamatInstitusi;
+            PubKeyInstitutionInfoMap[blockAddress].nomorTelepon = _nomorTelepon;
+
+
+            //store address to array
+            institutionBlockAddressArray.push(blockAddress);
+
+            return true;
+
         }
     
-    function isExistInstitution(uint _NPSN) public view returns (bool) {
-        for (uint i=0; i <= institutionInfoMap.length; i++) {
-            if (institutionInfoMap[i].NPSN == _NPSN) {
+    // function isExistInstitution(uint _NPSN) public view returns (bool) {
+    //     for (uint i=0; i <= institutionInfoMap.length; i++) {
+    //         if (institutionInfoMap[i].NPSN == _NPSN) {
+    //             return true;
+    //         }
+    //     return false;
+    //     }
+
+    function isExistInstitution(address _blockAddress) public view returns (bool) {
+        for (uint i=0; i <= institutionBlockAddressArray.length; i++) {
+            if (_blockAddress == institutionBlockAddressArray[i]) {
                 return true;
             }
-        return false;
+            return false;
         }
     }
 }
 
-contract RSSC {
-
-}
 
 contract RPCC {
     
+}
+
+contract RSSC {
+
 }
