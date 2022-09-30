b9c387315a2778f48388ac0f5dc52285e5b13baa
Dea Ivanka Malaha <rdv.malaha@gmaill.com>
2022-09-30 15:06:01 +0800

    add login, sign up, main dashboard with sidebar

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,14 @@ module.exports = {
   content: ['./public/**/*.{html,js}'],
   theme: {
     extend: {
-      fontFamily: {
-        'inter' : ['Inter', 'sans-serif'],
-      }
+      
+    },
+    fontFamily: {
+      'inter' : ['Inter', 'sans-serif'],
     },
     fontSize: {
+      text_xs: ['12px', '18px'],
+      text_sm: ['14px', '20px'],
       text_md: ['16px', '24px'],
       text_lg: ['18px', '28px'],
       text_xl: ['20px', '30px'],
@@ -23,6 +26,23 @@ module.exports = {
       semibold: 600,
       bold: 700,
     },
+    colors: {
+      'text-base': '#101828',
+      'text-darker': '#344054',
+      'white': '#FFFFFF',
+      'asphalt' : '#475467',
+      'primary-900': '#42307D',
+      'primary-700': '#6941C6',
+      'primary-600': '#7F56D9',
+      'primary-100': '#F2F4F7',
+      'gray-50': '#F9FAFB',
+      'gray-100': '#F2F4F7',
+      'gray-200': '#EAECF0',
+      'gray-300': '#D0D5DD',
+      'gray-500': '#667085',
+    }
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
